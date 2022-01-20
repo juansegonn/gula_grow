32be9cf3d7281013df7ebd1d964fdf5416df900e
juan gonzalez <juansegonn@gmail.com>
2022-01-20 13:36:08 -0300

    agrgando navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import react from "react";
+import './NavBar.css';
+import logo from './src/img/logo-baner.png';
+
+export default function NavBar() {
+    return  (
+            <header>
+                <div className="logo-header">
+                    <a title="Home" href="index.html"><img src={logo}/></a>
+                </div>
+                <nav className="nav-menu">
+                    <ul>
+                        <li><a className="enlace" href="semillas.html">semillas</a></li>
+                        <li><a className="enlace" href="galeria.html">galeria</a></li>
+                        <li><a className="enlace" href="contacto.html">contacto</a></li>
+                    </ul>
+                </nav>
+            </header>
+            )
+}
