8b68b1f4effdc250bc39d3447c2ca3b8fb627548
juan gonzalez <juansegonn@gmail.com>
2022-02-09 16:30:20 -0300

    HACIENDO ROUTING

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,22 +2,25 @@ import { useEffect, useState } from "react";
 import { getProducts } from "../../api/api";
 import './ItemDetailContainer.css'; 
 import ItemDetail from "../ItemDetail/ItemDetail"
+import { useParams } from "react-router-dom";
 
 function ItemDetailContainer (){
-    const[producto, setProducto] = useState ([])
+    const[producto, setProducto] = useState ()
+    const { productoId } = useParams()
 
     useEffect (() => {
+        
         getProducts().then((productos) => {
-           const producto = productos.find((i) => i.id === 1)
-           console.log(producto) 
+           const producto = productos.find((i) => i.id === Number(productoId))
            setProducto(producto)
-
+        }).catch((error) => {
+            console.log(error)
         })
-    }, [])
+    }, [productoId])
 
     return (
-        <div>
-            <ItemDetail producto = { producto }/>
+        <div className="item__detail__container">
+            {!producto ? <p>cargando producto...</p> : <ItemDetail producto = { producto }/>}
         </div>
     )
 }
