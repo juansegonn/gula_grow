931c982a2e287bf013a2546079271d9e400a86f9
juan gonzalez <juansegonn@gmail.com>
2022-02-21 02:34:13 -0300

    css al carrito

--- src/components/Cart/Cart,.js
+++ src/components/Cart/Cart,.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { Link } from 'react-router-dom';
+import './Cart.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const Cart = () => {
     const { cart, vaciarCarrito, deleteItem } = useContext(CartContext)
@@ -14,16 +16,22 @@ const Cart = () => {
                     <Link to="/">{'<<'}volver al inicio </Link>
                 </>
             ) : (
-                <>    
+                <div className="cart__container">    
                     {cart.map((producto)=> (
-                        <div key={producto.id}> 
-                            <h3>{producto.nombre}</h3>
-                            <h3>{producto.cantidad}</h3>
+                        <div className="cart__productos" key={producto.id}> 
+                            <div> 
+                                <img className="img__productos" src= { producto.img } alt= "imagen del producto"/>
+                            </div>
+                            <div> 
+                                <h3>{producto.nombre}</h3>
+                                <h3>{producto.cantidad}</h3>
+                                <h3>${producto.precio * producto.cantidad}</h3>
+                            </div>
                             <button onClick={() => deleteItem(producto.id)}>X</button>
                         </div>
                 ))}
                 <button onClick={ vaciarCarrito }> Vaciar carrito</button>
-            </> 
+            </div> 
             )}
         </>
     )
